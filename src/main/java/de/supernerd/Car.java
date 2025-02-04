df5ef141f2bf85fd1713c28dec05e71d7c16eef4
Manuel Simon <info@der-supernerd.de>
2025-02-04 10:54:48 +0100

    Implement method startEngine

--- src/main/java/de/supernerd/Car.java
+++ src/main/java/de/supernerd/Car.java
@@ -1,5 +1,15 @@
 package de.supernerd;
 
 public class Car {
+    private String brand;
+    private String model;
+    private String color;
+    private int yearOfManufacture;
+    private int numberOfDoors;
+    private String engine;
+    private boolean caprio;
 
+    public void startEngine() {
+        System.out.println("Motor wurde gestartet!");
+    }
 }
